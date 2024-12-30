df0129a806e965ad69ce6caa8783fc882f499f3c
adironca <adironca@gmail.com>
2024-12-29 17:43:41 -0800

    Enabled token control on APIs endpoints.

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,5 @@
+. ~/.searchdisplay.env.sh
 curl -X POST http://127.0.0.1:7900/api/upload_document \
+  -H "Authorization: Token ${WEBSERVICE_API_TOKEN}" \
   -F "file=@$HOME/tmp/hill.jpg" \
   -F "converted_text=Another record for the hill $(date '+%Y%m%d-%H%M%s')."
